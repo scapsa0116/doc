7ac4a19032bf3b3df094ad511bafc857bf4962c2
stela <stela.capsa@gmail.com>
2021-06-21 17:39:17 -0500

    adding sessions controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,48 @@
+#log in log ou omniauth
+class SessionsController < ApplicationController
+    def home
+    end 
+     
+    def new
+       @user = User.new
+    end
+ 
+    def create
+       user = User.find_by(username: params[:user][:username])
+       if user && user.authenticate(params[:user][:password])
+          session[:user_id] = user.id 
+          redirect_to user_path(user)
+       else
+          flash[:message] = "incorrect username or password"
+          redirect_to '/login'
+       end
+    end 
+ 
+ 
+    def destroy
+      session.clear
+      redirect_to root_path
+    end
+ 
+ 
+    def google
+      
+      @user = User.find_or_create_by(email: auth["info"]["email"]) do |user|
+       user.username = auth["info"]["username"]
+       user.password = SecureRandom.hex(10)
+      end
+    if @user.save
+       session[:user_id] = @user.id
+       redirect_to user_path(@user)
+    else
+       redirect_to '/'
+    end
+    end 
+ 
+    private
+    
+    def auth
+       request.env['omniauth.auth']
+     end
+ 
+ end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users 
+
+  get "/auth/google_oauth2/callback" => 'sessions#google'
+
 end
